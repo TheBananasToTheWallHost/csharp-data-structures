add1694e62dacb80def6505eed9cb913a73c584a
Ryan Mileris <ryanmileris@yahoo.com>
2021-01-13 15:48:09 -0700

    finished documenting the CompBinaryHeap class

--- Structures/BinaryHeap/CompBinaryHeap.cs
+++ Structures/BinaryHeap/CompBinaryHeap.cs
@@ -8,6 +8,12 @@ namespace BananaTurtles.CSharp.DataStructures.Heaps
     {
         private IComparer<T> _comparer;
 
+        /// <summary>
+        /// Changes the value of the item at <paramref name="index"/> to <paramref name="newValue"/>.
+        /// </summary>
+        /// <param name="index">The index to change the value of.</param>
+        /// <param name="newValue">The new value for the heap item at <paramref name="index"/>.</param>
+        /// <exception cref="ArgumentOutOfRangeException"/>
         public override void ChangeValue(int index, T newValue)
         {
             if (!IsValidIndex(index))
@@ -31,6 +37,12 @@ namespace BananaTurtles.CSharp.DataStructures.Heaps
             }
         }
 
+        /// <summary>
+        /// Changes the value of the first occurence of <paramref name="oldValue"/> to <paramref name="newValue"/>.
+        /// </summary>
+        /// <param name="oldValue">The value currently in the heap that will be updated to <paramref name="newValue"/></param>
+        /// <param name="newValue">The new value for the first occurence of <paramref name="oldValue"/>.</param>
+        /// <exception cref="ArgumentException"/>
         public override void ChangeValue(T oldValue, T newValue)
         {
             int valueIndex = Array.IndexOf(_heapArray, oldValue);
@@ -43,6 +55,17 @@ namespace BananaTurtles.CSharp.DataStructures.Heaps
             ChangeValue(valueIndex, newValue);
         }
 
+        /// <summary>
+        /// Decreases the value of the item at <paramref name="index"/> to <paramref name="newValue"/>. 
+        /// The <paramref name="newValue"/> must be smaller than the value currently at <paramref name="index"/> relative
+        /// to the <see cref="IComparer{T}"/> used by the <see cref="CompBinaryHeap{T}"/>.
+        /// </summary>
+        /// <param name="index">The index to change the value of.</param>
+        /// <param name="newValue">The new value for the heap item at <paramref name="index"/>. This value must be smaller than 
+        /// the value currently at <paramref name="index"/> relative to the <see cref="IComparer{T}"/> used by the 
+        /// <see cref="CompBinaryHeap{T}"/>.</param>
+        /// <exception cref="ArgumentOutOfRangeException"/>
+        /// <exception cref="ArgumentException"/>
         public override void DecreaseValue(int index, T newValue)
         {
             if (!IsValidIndex(index))
@@ -54,13 +77,23 @@ namespace BananaTurtles.CSharp.DataStructures.Heaps
 
             if (_comparer.Compare(newValue, oldValue) >= 0)
             {
-                throw new ArgumentException($"The value of {nameof(newValue)} is not smaller than the value at {index}");
+                throw new ArgumentException($"The value of {nameof(newValue)} is not smaller than the value at {index}.");
             }
 
             _heapArray[index] = newValue;
             BubbleUp(index);
         }
 
+        /// <summary>
+        /// Decreases the value of the first occurrence of <paramref name="oldValue"/> to <paramref name="newValue"/>.
+        /// The <paramref name="newValue"/> must be smaller than the <paramref name="oldValue"/> relative to the 
+        /// <see cref="IComparer{T}"/> used by the <see cref="CompBinaryHeap{T}"/>.
+        /// </summary>
+        /// <param name="oldValue">The value currently in the heap that will be updated to <paramref name="newValue"/></param>
+        /// <param name="newValue">The new value for the first occurence of <paramref name="oldValue"/>. This value must be smaller than 
+        /// <paramref name="oldValue"/> relative to the <see cref="IComparer{T}"/> used by the <see cref="CompBinaryHeap{T}"/>.</param>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="InvalidOperationException"/>
         public override void DecreaseValue(T oldValue, T newValue)
         {
             int valueIndex = Array.IndexOf(_heapArray, oldValue);
@@ -72,18 +105,23 @@ namespace BananaTurtles.CSharp.DataStructures.Heaps
 
             if (valueIndex == -1)
             {
-                throw new ArgumentException($"No item matching {oldValue} was found.");
+                throw new InvalidOperationException($"No item matching {oldValue} was found.");
             }
 
             DecreaseValue(valueIndex, newValue);
         }
 
         /// <summary>
-        /// Increases the value of the item at <paramref name="index"/> to <paramref name="newValue"/> relative to the 
-        /// <see cref="IComparer{T}"/> being used by the <see cref="CompBinaryHeap{T}"/>.
+        /// Increases the value of the item at <paramref name="index"/> to <paramref name="newValue"/>. 
+        /// The <paramref name="newValue"/> must be larger than the value currently at <paramref name="index"/> relative
+        /// to the <see cref="IComparer{T}"/> used by the <see cref="CompBinaryHeap{T}"/>.
         /// </summary>
-        /// <param name="index"></param>
-        /// <param name="newValue"></param>
+        /// <param name="index">The index to change the value of.</param>
+        /// <param name="newValue">The new value for the heap item at <paramref name="index"/>. This value must be larger than 
+        /// the value currently at <paramref name="index"/> relative to the <see cref="IComparer{T}"/> used by the 
+        /// <see cref="CompBinaryHeap{T}"/>.</param>
+        /// <exception cref="ArgumentOutOfRangeException"/>
+        /// <exception cref="ArgumentException"/>
         public override void IncreaseValue(int index, T newValue)
         {
             if (!IsValidIndex(index))
@@ -102,6 +140,16 @@ namespace BananaTurtles.CSharp.DataStructures.Heaps
             Heapify(index);
         }
 
+        /// <summary>
+        /// Increases the value of the first occurrence of <paramref name="oldValue"/> to <paramref name="newValue"/>.
+        /// The <paramref name="newValue"/> must be larger than the <paramref name="oldValue"/> relative to the 
+        /// <see cref="IComparer{T}"/> used by the <see cref="CompBinaryHeap{T}"/>.
+        /// </summary>
+        /// <param name="oldValue">The value currently in the heap that will be updated to <paramref name="newValue"/></param>
+        /// <param name="newValue">The new value for the first occurence of <paramref name="oldValue"/>. This value must be larger than 
+        /// <paramref name="oldValue"/> relative to the <see cref="IComparer{T}"/> used by the <see cref="CompBinaryHeap{T}"/>.</param>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="InvalidOperationException"/>
         public override void IncreaseValue(T oldValue, T newValue)
         {
             int valueIndex = Array.IndexOf(_heapArray, oldValue);
@@ -113,12 +161,19 @@ namespace BananaTurtles.CSharp.DataStructures.Heaps
 
             if (valueIndex == -1)
             {
-                throw new ArgumentException($"No item matching {oldValue} was found.");
+                throw new InvalidOperationException($"No item matching {oldValue} was found.");
             }
 
             IncreaseValue(valueIndex, newValue);
         }
 
+        /// <summary>
+        /// Changes the value of the item at <paramref name="index"/> to <paramref name="newValue"/>. The return value indicates
+        /// whether the change operation succeeded.
+        /// </summary>
+        /// <param name="index">The index to change the value of.</param>
+        /// <param name="newValue">The new value for the heap item at <paramref name="index"/>.</param>
+        /// <returns>True if the value at <paramref name="index"/> was successfully changed, false otherwise.</returns>
         public override bool TryChangeValue(int index, T newValue)
         {
             try
@@ -132,6 +187,13 @@ namespace BananaTurtles.CSharp.DataStructures.Heaps
             }
         }
 
+        /// <summary>
+        /// Changes the value of the first occurence of <paramref name="oldValue"/> to <paramref name="newValue"/>. 
+        /// The return value indicates whether the change operation succeeded.
+        /// </summary>
+        /// <param name="oldValue">The value currently in the heap that will be updated to <paramref name="newValue"/></param>
+        /// <param name="newValue">The new value for the first occurence of <paramref name="oldValue"/>.</param>
+        /// <returns>True if <paramref name="oldValue"/> was successfully found and changed, false otherwise.</returns>
         public override bool TryChangeValue(T oldValue, T newValue)
         {
             try
